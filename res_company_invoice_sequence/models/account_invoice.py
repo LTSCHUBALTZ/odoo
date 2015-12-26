0426793e431e380126e374888c919d3d29da45f8
Hector Manuel Aular Osorio <aular.hector3@gmail.com>
2015-12-26 02:11:21 -0430

    La secuencia solo aplica a factura de ciente

--- res_company_invoice_sequence/models/account_invoice.py
+++ res_company_invoice_sequence/models/account_invoice.py
@@ -39,7 +39,7 @@ class AccountInvoice(models.Model):
     @api.multi
     def invoice_validate(self):
         for invoice in self:
-	    if invoice.information_company_id.sequence_id.id:
+	    if invoice.information_company_id.sequence_id.id and invoice.type == "out_invoice":
                 invoice.invoice_control_number = invoice.information_company_id.sequence_id.next_by_id() 
 
         return super(AccountInvoice, self).invoice_validate()
