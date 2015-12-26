1df98101502cffca628dcdad90eb72a4c1670719
Hector Manuel Aular Osorio <aular.hector3@gmail.com>
2015-12-26 01:26:31 -0430

    Nuevo modulo que instala todos los modulos desarrollados en este repositorio

---
+++ all_install/README.md
@@ -0,0 +1,5 @@
+All Install
+-----------
+
+Install all modules of Ricardo
+
---
+++ all_install/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Developed by: thinkasoft , C.A.
+#    Coded by: Aular Hector Manuel (aular.hector3@gmail.com)
+#
+##############################################################################
+{
+    "name": "Install All Modules",
+    "summary": """
+""",
+    "description": """
+Ricardo Modules to install
+    """,
+    "author": "Disprotec SRL & Ingenieria Thinkasoft de Venezuela",
+    "website": "http://www.disprotec.net",
+    "category": "Accounting & Finance",
+    "version": "0.1",
+    "depends": [
+	"res_company_info_numbers",
+	"res_company_invoice_sequence",
+	"account_invoice_partner_wizard",
+    ],
+}
